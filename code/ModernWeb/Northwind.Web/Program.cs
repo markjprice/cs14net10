6a626e773b88c6b2d23d1b9a68feb88a91e17fb0
Mark J Price <markjprice@gmail.com>
2025-09-15 09:30:44 +0100

    Update HTTP pipeline section

--- code/ModernWeb/Northwind.Web/Program.cs
+++ code/ModernWeb/Northwind.Web/Program.cs
@@ -19,6 +19,32 @@ if (!app.Environment.IsDevelopment())
   app.UseHsts();
 }
 
+// Implementing an anonymous inline delegate as middleware
+// to intercept HTTP requests and responses.
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+  RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+
+  if (rep is not null)
+  {
+    WriteLine($"Endpoint name: {rep.DisplayName}");
+    WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+  }
+
+  if (context.Request.Path == "/bonjour")
+  {
+    // In the case of a match on URL path, this becomes a terminating
+    // delegate that returns so does not call the next delegate.
+    await context.Response.WriteAsync("Bonjour Monde!");
+    return;
+  }
+
+  // We could modify the request before calling the next delegate.
+  await next();
+
+  // We could modify the response after calling the next delegate.
+});
+
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();

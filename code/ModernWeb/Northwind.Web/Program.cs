a9dc1c016b2280df906529336180a3d7c1bb799b
Mark J Price <markjprice@gmail.com>
2025-09-22 16:01:13 +0100

    Update Program.cs

--- code/ModernWeb/Northwind.Web/Program.cs
+++ code/ModernWeb/Northwind.Web/Program.cs
@@ -70,11 +70,21 @@ app.MapGet("/welcome", () => Results.Content(
   <!doctype html>
   <html lang="en">
   <head>
-    <title>Welcome to Northwind Web!</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="site.css" rel="stylesheet" />
+    <title>About Northwind Web</title>
   </head>
   <body>
-    <h1>Welcome to Northwind Web!</h1>
-  </body>
+    <div class="container">
+      <div class="jumbotron">
+        <h1 class="display-3">About Northwind Web</h1>
+        <p class="lead">We supply products to our customers.</p>
+        <img src="categories.jpeg" style="height:200px;width:300px;" />
+      </div>
+    </div>
+    </body>
   </html>
   """,
   contentType: "text/html"));
